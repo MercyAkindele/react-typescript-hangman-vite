769cde8c0a32d09e9b8b01b1e4ea900316775da9
Mercy Akindele <mercyakinlawon@gmail.com>
2023-09-26 18:55:44 -0500

    initial upload

---
+++ src/App.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useState, useCallback } from "react";
+import HangmanDrawing from "./components/HangmanDrawing";
+import HangmanWord from "./components/HangmanWord";
+import Keyboard from "./components/Keyboard";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(" ");
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
+  const isLoser = incorrectLetters.length >= 6;
+
+  const isWinner = wordToGuess
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+    },
+    [guessedLetters, isLoser, isWinner]
+  );
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z-]$/)) return;
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+    document.addEventListener("keypress", handler);
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
+
+  useEffect(() => {
+    const fetchWord = async () => {
+      const url = "https://wordsapiv1.p.rapidapi.com/words/?random=true";
+      const options: RequestInit = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "cb85743adcmshef6dbf43d458c7fp1b7717jsn9f4c88a21e54",
+          "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+        },
+      };
+      try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        const word = data.word.split(" ")[0];
+        setWordToGuess(word);
+      } catch (error) {
+        console.error("Error fetching word: ", error);
+      }
+    };
+
+    fetchWord();
+  }, []);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        {isWinner && "Winner! Refresh to try another word!"}
+        {isLoser && "Nice try! Refresh page to try a different word!"}
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
+      <div
+        style={{
+          alignSelf: "stretch",
+        }}
+      >
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter((letter) =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HangmanWord.tsx
@@ -0,0 +1,42 @@
+type HangmanWordProps = {
+  reveal?: boolean;
+  wordToGuess: string;
+  guessedLetters: string[];
+};
+const HangmanWord = ({
+  reveal = false,
+  wordToGuess,
+  guessedLetters,
+}: HangmanWordProps) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) && reveal ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default HangmanWord;
